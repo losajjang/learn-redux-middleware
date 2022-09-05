c76662c0d7e98a6a65a303ee3bd68fc8414ccb3a
losajjang <losajjang@naver.com>
2022-09-05 23:02:57 +0900

    redux-saga 로 프로미스 다루기

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,49 +1,81 @@
-// Promise 에 기반한 Thunk 를 만들어 주는 함수입니다.
-export const createPromiseThunk = (type, promiseCreator) => {
-  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+import {call, put} from 'redux-saga/effects';
+/* redux-thunk */
+// // Promise 에 기반한 Thunk 를 만들어 주는 함수입니다.
+// export const createPromiseThunk = (type, promiseCreator) => {
+//   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+//   // 이 함수는 promiseCreator 가 단 하나의 파라미터만 받는다는 전제하에 작성되었습니다.
+//   // 만약 여러 종류의 파라미터를 전달해야 하는 상황에서는 객체 타입의 파라미터를 받아오도록 하면 됩니다.
+//   // 예. writeComment({postId:1, text:'댓글 내용'})
+//   return param => async dispatch => {
+//     // 요청 시작
+//     dispatch({type, param});
+//     try {
+//       // 결과물의 이름을 payload 라는 이름으로 통일시킵니다.
+//       const payload = await promiseCreator(param);
+//       dispatch({type: SUCCESS, payload}); // 성공
+//     } catch (e) {
+//       dispatch({type: ERROR, payload: e, error: true}); // 실패
+//     }
+//   };
+// };
 
-  // 이 함수는 promiseCreator 가 단 하나의 파라미터만 받는다는 전제하에 작성되었습니다.
-  // 만약 여러 종류의 파라미터를 전달해야 하는 상황에서는 객체 타입의 파라미터를 받아오도록 하면 됩니다.
-  // 예. writeComment({postId:1, text:'댓글 내용'})
-  return param => async dispatch => {
-    // 요청 시작
-    dispatch({type, param});
+// // 특정 id 를 처리하는 Thunk 생성 함수
+// const defaultIdSelector = param => param;
+// export const createPromiseThunkById = (
+//   type,
+//   promiseCreator,
+//   // 파라미터에서 id 를 어떻게 선택할 지 정의하는 함수입니다.
+//   // 기본값으로는 파라미터를 그대로 id 로 사용합니다.
+//   // 하지만 만약 파라미터가 {id:1, details:true} 이런 형태라면
+//   // idSelector 를 param => param.id 이런식으로 설정할 수 있겠죠.
+//   idSelector = defaultIdSelector,
+// ) => {
+//   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+//   return param => async dispatch => {
+//     const id = idSelector(param);
+//     dispatch({type, meta: id});
+//     try {
+//       const payload = await promiseCreator(param);
+//       dispatch({type: SUCCESS, payload, meta: id});
+//     } catch (e) {
+//       dispatch({type: ERROR, error: true, payload: e, meta: id});
+//     }
+//   };
+// };
+
+/* redux-saga */
+// 프로미스를 기다렸다가 결과를 디스패치하는 사가
+export const createPromiseSaga = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return function* saga(action) {
     try {
-      // 결과물의 이름을 payload 라는 이름으로 통일시킵니다.
-      const payload = await promiseCreator(param);
-      dispatch({type: SUCCESS, payload}); // 성공
+      // 재사용성을 위하여 promiseCreator 의 파라미터엔 action.payload 값을 넣도록 설정합니다.
+      const payload = yield call(promiseCreator, action.payload);
+      yield put({type: SUCCESS, payload});
     } catch (e) {
-      dispatch({type: ERROR, payload: e, error: true}); // 실패
+      yield put({type: ERROR, error: true, payload: e});
     }
   };
 };
 
-// 특정 id 를 처리하는 Thunk 생성 함수
-const defaultIdSelector = param => param;
-export const createPromiseThunkById = (
-  type,
-  promiseCreator,
-  // 파라미터에서 id 를 어떻게 선택할 지 정의하는 함수입니다.
-  // 기본값으로는 파라미터를 그대로 id 로 사용합니다.
-  // 하지만 만약 파라미터가 {id:1, details:true} 이런 형태라면
-  // idSelector 를 param => param.id 이런식으로 설정할 수 있겠죠.
-  idSelector = defaultIdSelector,
-) => {
+// 특정 id 의 데이터를 조회하는 용도로 사용하는 사가
+// API 를 호출할 때 파라미터는 action.payload 를 넣고,
+// id 값을 action.mete 로 설정합니다.
+export const createPromiseSagaById = (type, promiseCreator) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
-
-  return param => async dispatch => {
-    const id = idSelector(param);
-    dispatch({type, meta: id});
+  return function* saga(action) {
+    const id = action.meta;
     try {
-      const payload = await promiseCreator(param);
-      dispatch({type: SUCCESS, payload, meta: id});
+      const payload = yield call(promiseCreator, action.payload);
+      yield put({type: SUCCESS, payload, meta: id});
     } catch (e) {
-      dispatch({type: ERROR, error: true, payload: e, meta: id});
+      yield put({type: ERROR, error: true, meta: id});
     }
   };
 };
 
-// 리듀서에서 사용할 수 있는 여러 유틸 함수들입니다.
+/* 리듀서에서 사용할 수 있는 여러 유틸 함수들입니다. */
 export const reducerUtils = {
   // 초기 상태. 초기 data 값은 기본적으로 null 이지만
   // 바꿀 수도 있습니다.
@@ -73,7 +105,7 @@ export const reducerUtils = {
   }),
 };
 
-// 비동기 관련 액션들을 처리하는 리듀서를 만들어 줍니다.
+/* 비동기 관련 액션들을 처리하는 리듀서를 만들어 줍니다. */
 // type 은 액션의 타입 key 는 상태의 key (예: posts, post) 입니다.
 export const handleAsyncActions = (type, key, keepData = false) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,8 @@
+import {call, put, takeEvery} from 'redux-saga/effects';
 import * as postsAPI from '../api/posts';
 import {
+  createPromiseSaga,
+  createPromiseSagaById,
   createPromiseThunk,
   createPromiseThunkById,
   handleAsyncActionById,
@@ -7,7 +10,7 @@ import {
   reducerUtils,
 } from '../lib/asyncUtils';
 
-// 액션
+/* 액션 */
 // 포스트 여러개 조회하기
 const GET_POSTS = 'GET_POSTS'; // 요청 시작
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; // 요청 성공
@@ -18,16 +21,19 @@ const GET_POST = 'GET_POST'; // 요청 시작
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS'; // 요청 성공
 const GET_POST_ERROR = 'GET_POST_ERROR'; // 요청 실패
 
-// 포스트 비우기
-const CLEAR_POST = 'CLEAR_POST';
+// // 포스트 비우기
+// const CLEAR_POST = 'CLEAR_POST';
 
-// 액션 생성 함수
-export const clearPost = () => ({type: CLEAR_POST});
+/* 액션 생성 함수 */
+// export const clearPost = () => ({type: CLEAR_POST});
 export const goHome = navigate => (dispatch, getState) => {
   navigate('/');
 };
+export const getPosts = () => ({type: GET_POSTS});
+// payload 는 파라미터 용도, meta 는 리듀서에서 id 를 알기위한 용도
+export const getPost = id => ({type: GET_POST, payload: id, meta: id});
 
-// // 미들웨어
+/* 미들웨어 (thunk) */
 // // thunk 를 사용할 때, 꼭 모든 액션들에 대해 액션 생성 함수를 만들 필요는 없습니다.
 // // 그냥 thunk 함수에서 바로 액션 객체를 만들어 주어도 괜찮습니다.
 // export const getPosts = () => async dispatch => {
@@ -51,17 +57,65 @@ export const goHome = navigate => (dispatch, getState) => {
 //   }
 // };
 
-// 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
-export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+// // 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
+// export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+// export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
 
-// 초기값
+/* 미들웨어 (saga) */
+// function* getPostsSaga() {
+//   try {
+//     const posts = yield call(postsAPI.getPosts); // call 을 사용하면 특정 함수를 호출하고, 결과물이 반환될 때까지 기다려 줄 수 있습니다.
+//     yield put({
+//       type: GET_POSTS_SUCCESS,
+//       payload: posts,
+//     }); // 성공 액션 디스패치
+//   } catch (e) {
+//     yield put({
+//       type: GET_POSTS_ERROR,
+//       error: true,
+//       payload: e,
+//     }); // 실패 액션 디스패치
+//   }
+// }
+
+// function* getPostSaga(action) {
+//   const param = action.payload;
+//   const id = action.meta;
+//   try {
+//     const post = yield call(postsAPI.getPostById, param); // API 함수에 넣어주고 싶은 인자는 call 함수의 두번째 인자부터 순서대로 넣어주면 됩니다.
+//     yield put({
+//       type: GET_POST_SUCCESS,
+//       payload: post,
+//       meta: id,
+//     });
+//   } catch (e) {
+//     yield put({
+//       type: GET_POST_ERROR,
+//       error: true,
+//       payload: e,
+//       meta: id,
+//     });
+//   }
+// }
+
+/* saga 리팩토링 */
+// 아주 쉽게 saga 를 만들 수 있게 되었습니다.
+const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
+const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
+
+// 사가들을 합치기
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+}
+
+/* 초기값 */
 const initialState = {
   posts: reducerUtils.initial(),
   post: {},
 };
 
-// // 리듀서
+/* 리듀서 */
 // export default function posts(state = initialState, action) {
 //   switch (action.type) {
 //     case GET_POSTS:
